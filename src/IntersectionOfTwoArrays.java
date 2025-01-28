5fdd41dc2d45c2aabfd069479c11a2c019528aba
erica-arroyo-a <eaguil8@hotmail.com>
2025-01-27 16:14:41 -0800

    sample problem

---
+++ src/IntersectionOfTwoArrays.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their
+ * intersection. Each element in the result must be unique and you may return the result in any order.
+ *
+ * Example 1:
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ *
+ * Example 2:
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Explanation: [4,9] is also accepted.
+ */
+public class IntersectionOfTwoArrays {
+    /**
+     * Use two hash sets
+     * Time complexity: O(n)
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> res = new HashSet<>();
+
+        for(int i=0; i<nums1.length; i++){
+            set.add(nums1[i]);
+        }
+
+        for(int i=0; i<nums2.length; i++){
+            if(set.contains(nums2[i])){
+                res.add(nums2[i]);
+            }
+        }
+        int[] result = new int[res.size()];
+        int index = 0;
+        for (int num : res) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+
+    /**
+     * Sort both arrays, use two pointers
+     * Time complexity: O(nlogn)
+     */
+    public int[] intersectionV2(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+
+    /**
+     * Binary search
+     * Time complexity: O(nlogn)
+     */
+    public int[] intersectionV3(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
